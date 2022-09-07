4e09e606a6b0fbf583f54c94eebc2e1174316643
Slav <itiksolution@gmail.com>
2022-09-07 11:57:05 +1000

    assignment complete

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const requestURL = 'https://anapioficeandfire.com/api/books'
 
 function fetchBooks() {
-  return fetch(requestURL)  /** DONT FORGET RETURN!!!! */
-.then(resp => resp.json())
-.then(json => renderBooks(json))
-  
+  return fetch(requestURL) /** DONT FORGET RETURN!!!! */
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  const main = document.querySelector('main')
+  books.forEach((book) => {
+    const p = document.createElement('p')
+    p.innerText = book.name
+    main.appendChild(p)
+  })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks()
+})
+
